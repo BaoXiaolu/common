9c7741fe1794f3313ca51ed4c330fb598d2e19c6
Charlotte Bao <baoxiaolumail@gmail.com>
2017-02-02 20:55:24 +0800

    修改ApplicationUtil

--- src/main/java/com/bxl/common/util/ApplicationUtil.java
+++ src/main/java/com/bxl/common/util/ApplicationUtil.java
@@ -10,6 +10,8 @@
  */
 package com.bxl.common.util;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.Map;
@@ -37,9 +39,9 @@ public class ApplicationUtil implements Serializable{
      * 产生一个不包含"-"的UUID
      * @return
      */
-    public static String randomUUIDStr() {
-        return UUID.randomUUID().toString().replace("-", "");
-    }
+//    public static String randomUUIDStr() {
+//        return UUID.randomUUID().toString().replace("-", "");
+//    }
 
     /**
      * 获取当前时间戳
@@ -48,6 +50,36 @@ public class ApplicationUtil implements Serializable{
      */
     public static Timestamp currentTime() {
         return new Timestamp(System.currentTimeMillis());
+    }
+
+    /**
+     * md5加密
+     *
+     * @param value 要加密的值
+     * @return md5加密后的值
+     */
+    public static String md5Hex(String value) {
+        return DigestUtils.md5Hex(value);
+    }
+
+    /**
+     * sha1加密
+     *
+     * @param value 要加密的值
+     * @return sha1加密后的值
+     */
+    public static String sha1Hex(String value) {
+        return DigestUtils.sha1Hex(value);
+    }
+
+    /**
+     * sha256加密
+     *
+     * @param value 要加密的值
+     * @return sha256加密后的值
+     */
+    public static String sha256Hex(String value) {
+        return DigestUtils.sha256Hex(value);
     }
 
     public static <T> Predicate<T> distinctByKey(Function<? super T,Object> keyExtractor) {

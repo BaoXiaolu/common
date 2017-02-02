329ed19f7978fe1eebbddb86644910d7fcdb8f9a
Charlotte Bao <baoxiaolumail@gmail.com>
2017-02-02 09:42:24 +0800

    增加用以模拟枚举的抽象类GenericEnum

---
+++ src/main/java/com/bxl/commons/generic/GenericEnum.java
@@ -0,0 +1,58 @@
+/*
+ * commons - com.bxl.commons.generic - GenericEnum        
+ *
+ * @author Bao Xiaolu
+ * @contact baoxiaolumail@gmail.com
+ * @date 2017/2/1
+ * 
+ * Copyright © https://github.com/CharlotteBao
+ * All rights reserved.
+ */
+package com.bxl.commons.generic;
+
+import java.io.Serializable;
+
+/**
+ * 枚举Fake抽象类.
+ * 以pojo类代替枚举对象进行操作.
+ *
+ * @param <T> 枚举值类型
+ */
+public abstract class GenericEnum<T> implements Serializable {
+    /**
+     * 枚举文本
+     */
+    private String text;
+    /**
+     * 枚举值
+     */
+    private T value;
+    /**
+     * 枚举描述
+     */
+    private String description;
+
+    /**
+     *
+     * @param text 枚举文本
+     * @param value 枚举值
+     * @param description 枚举描述
+     */
+    public GenericEnum(String text, T value, String description) {
+        this.text = text;
+        this.value = value;
+        this.description = description;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
